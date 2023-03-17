a4dc1b75cf5d44498d061e49d67c1c6be3fbf898
WoodHolz <1535980162@qq.com>
2023-03-17 21:37:10 +0800

    2023/3/17

---
+++ lanqiao/day_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+    {
+        int n = 0;
+        cin >> n;
+        for(int i = 0; i <= n; i++){
+            /*
+            if(pow(n / 100, 3) /
+            + pow(n % 100 / 10, 3) /
+            + pow(n % 10, 3) == i){
+                //cout << i << endl;
+                cout << "XD" << endl;
+                
+            }
+            */
+            cout << "XD" << endl;
+        }
+        return EXIT_SUCCESS;
+    }
